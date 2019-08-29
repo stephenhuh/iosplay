1711535eb7d2ce4ef2b44a9b85c4580be5916378
stevie huh <stevie.huh@bcgdv.com>
2019-08-28 20:30:42 -0400

    add some basic logic to facts

--- RandomFacts/ViewController.swift
+++ RandomFacts/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var randomFactLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func showAnotherFunFact(_ sender: Any) {
+        let facts = ["Ostriches can run faster than horses",
+                     "It takes about 8 minutes for the light from the Sun to reach Earth",
+                     "Ants stretch in the morning when they wake up",
+                     "On average, it takes 66 days to form a new habit"]
+        let randomNumberIdx = Int.random(in: 0 ... facts.count - 1)
+        randomFactLabel.text = facts[randomNumberIdx]
+    }
 }
 
