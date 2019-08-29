5d52e07e8a0f7e03b62d64c755935f1e930118fb
stevie huh <stevie.huh@bcgdv.com>
2019-08-28 21:04:36 -0400

    responsiveness and ui changes

--- RandomFacts/ViewController.swift
+++ RandomFacts/ViewController.swift
@@ -9,20 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     @IBOutlet weak var randomFactLabel: UILabel!
+    
+    let facts = ["Ostriches can run faster than horses",
+                 "It takes about 8 minutes for the light from the Sun to reach Earth",
+                 "Ants stretch in the morning when they wake up",
+                 "On average, it takes 66 days to form a new habit"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        randomFactLabel.text = facts[0]
     }
     
-    
     @IBAction func showAnotherFunFact(_ sender: Any) {
-        let facts = ["Ostriches can run faster than horses",
-                     "It takes about 8 minutes for the light from the Sun to reach Earth",
-                     "Ants stretch in the morning when they wake up",
-                     "On average, it takes 66 days to form a new habit"]
         let randomNumberIdx = Int.random(in: 0 ... facts.count - 1)
         randomFactLabel.text = facts[randomNumberIdx]
     }
